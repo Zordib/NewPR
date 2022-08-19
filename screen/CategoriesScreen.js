1ae6e347a5309a0515efd7bf99c03e6504a8e2f3
Peter Olaleru <olalerupeter@gmail.com>
2022-08-19 14:41:22 +0100

    formatted

--- screen/CategoriesScreen.js
+++ screen/CategoriesScreen.js
@@ -1,36 +1,32 @@
-import { CATEGORIES } from '../data/dummy-data'
-import { FlatList } from 'react-native'
-import CategoryGridTile from '../components/CategoryGridTile';
-
-
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 function CategoriesScreen({ navigation }) {
-    function renderCategoryItem(itemData) {
-        function pressHandler() {
-            navigation.navigate('MealsOverview', {
-                categoryId: "zord",
-
-
-            });
-        }
-        return (
-            <CategoryGridTile
-                title={itemData.item.title}
-                color={itemData.item.color}
-                onPress={pressHandler}
-            />
-        );
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     }
 
     return (
-        <FlatList
-            data={CATEGORIES}
-            keyExtractor={(item) => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
 
-        />
-    )
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
 }
 
 export default CategoriesScreen;
